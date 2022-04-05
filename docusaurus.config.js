d1b98c28c8aaebabd00d32effa23c196ab5f50a9
piercebrands <piercebrands@gmail.com>
2022-04-05 12:27:52 +0800

    change host

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'FastShell',
   tagline: 'Fast Shell supports the management of custom scripts, which can quickly execute shell commands. The supported commands depend on the shell environment used by the current IDE. Fast Shell has a fairly high degree of freedom. All instructions are completely defined by the user.',
   url: 'https://github.com/',
-  baseUrl: '/FastShell/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
